9624aa04f6adeefa51edd294e46b1c76a756b6ed
ravi688 <rp0412204@gmail.com>
2024-09-20 10:15:22 -0400

    Implemented the maximum sub array sum alog (O(n^2) and O(n) both)

--- maximum_subarray.cpp
+++ maximum_subarray.cpp
@@ -3,9 +3,42 @@
 // NOTE: 'auto' in template parameters types are permitted in C++17 and onwards,
 // Execute: g++ -std=c++14 <this_file.cpp> -o <output> would lead to syntax errors
 template<typename T, auto N>
-constexpr std::pair<decltype(N), decltype(N)> get_maximum_sub_array(const T (&array)[N]) noexcept
+constexpr T get_maximum_sub_array_sum1(const T (&array)[N]) noexcept
 {
-	return {  };
+	// Time Complexity: O(n^2)
+	T maxSum = array[0];
+	using SizeType = decltype(N);
+	for(SizeType i = 1; i <= N; ++i)
+	{
+		T sum = 0;
+		for(SizeType j = i; j < N; ++j)
+			sum += array[j];
+		for(SizeType j = i; j < N; ++j)
+			sum += (array[j] - array[j - i]);
+		if(sum > maxSum)
+			maxSum = sum;
+	}
+	return maxSum;
+}
+
+template<typename T, auto N>
+constexpr T get_maximum_sub_array_sum2(const T (&array)[N]) noexcept
+{
+	// Time Complexity: O(n)
+	using SizeType = decltype(N);
+	T currMax = array[0];
+	T globalMax = currMax;
+	for(SizeType i = 1; i < N; ++i)
+	{
+		// Explaination:
+		//	Here, it is possible that 'currMax + array[i]' is less than 'array[i]'
+		//	in which case, we have a gurantee that 'array[i]' can compensate for the next subarray,
+		// 	therefore, we reset the 'currMax' to 'array[i]' and start summing up again from there.
+		currMax = std::max(array[i], currMax + array[i]);
+		if(currMax > globalMax)
+			globalMax = currMax;
+	}
+	return globalMax;
 }
 
 template<typename T, auto N>
@@ -31,7 +64,9 @@ int main()
 {
 	int array[] = { -1, 2, 4, -3, 5, 2, -5, 2 };
 	std::cout << "Input: "; print_array(array);
-	std::pair<int, int> result = get_maximum_sub_array(array);
-	std::cout << "Output: " << result << std::endl;
+	int result1 = get_maximum_sub_array_sum1(array);
+	int result2 = get_maximum_sub_array_sum2(array);
+	std::cout << "Output 1: " << result1 << std::endl;
+	std::cout << "Output 2: " << result2 << std::endl;
 	return 0;
 }

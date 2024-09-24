359bd8f81fd4f80cff9e8c8baf365e6a52b82202
ravi688 <rp0412204@gmail.com>
2024-09-24 06:16:32 -0400

    Generalized the Set and now std::unordered_set can also be used in num_common_elements.cpp

--- num_common_elements.cpp
+++ num_common_elements.cpp
@@ -1,21 +1,33 @@
 #include <iostream> // for std::cout
 #include <vector> // for std::vector
 #include <set> // for std::set
+#include <unordered_set> // for std::unordered_set
 #include <concepts> // for std::convertible_to<> concept
+#include <utility> // for std::declval
 
 template<typename T>
-concept List = requires(const T& list)
+concept ReadOnlyList = requires(const T& list)
 {
 	{ list.size() } -> std::convertible_to<std::size_t>;
-	list.begin();
-	list.end();
+	list.cbegin();
+	list.cend();
 	typename T::value_type;
 };
 
-template<List T>
+template<template<typename> typename S, typename T>
+concept InsertableSet = requires(S<T>& set)
+{
+	typename S<T>::value_type;
+	set.insert(std::declval<T>());
+	{ set.contains(std::declval<T>()) } -> std::convertible_to<bool>;
+} && std::same_as<typename S<T>::value_type, T>;
+
+// NOTE: 'InsertableSet<typename T::value_type> S' work here (for some reason which I'm not aware of)
+// Therefore, I'm using requires instead.
+template<ReadOnlyList T, template<typename> class S> requires InsertableSet<S, typename T::value_type>
 auto calculate_num_common_elements(const T& list1, const T& list2) noexcept
 {
-	std::set<typename T::value_type> checkReg;
+	S<typename T::value_type> checkReg;
 	for(auto value : list1)
 		checkReg.insert(value);
 	decltype(list1.size()) count = 0;
@@ -25,7 +37,7 @@ auto calculate_num_common_elements(const T& list1, const T& list2) noexcept
 	return count;
 }
 
-template<List T>
+template<ReadOnlyList T>
 static std::ostream& operator<<(std::ostream& stream, const T& list) noexcept
 {
 	for(decltype(list.size()) i = 0; auto value : list)
@@ -44,7 +56,13 @@ int main()
 	std::vector<int> list2 = { 3, 2, 9, 5 };
 	std::cout << "List1: " << list1 << std::endl;
 	std::cout << "List2: " << list2 << std::endl;
-	auto count = calculate_num_common_elements(list1, list2);
-	std::cout << "Result: " << count << std::endl;
+	{
+		auto count = calculate_num_common_elements<decltype(list1), std::set>(list1, list2);
+		std::cout << "Result (set): " << count << std::endl;
+	}
+	{
+		auto count = calculate_num_common_elements<decltype(list1), std::unordered_set>(list1, list2);
+		std::cout << "Result (unordered_set): " << count << std::endl;
+	}
 	return 0;
 };

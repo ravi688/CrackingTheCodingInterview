387e2064e12f6953947b62697608bedd4e28eed2
ravi688 <rp0412204@gmail.com>
2024-09-25 06:26:09 -0400

    Added timing to demonstrat std::unordered_set is faster than std::set for larger number of inputs  - NOTE: std::set can be faster than std::unordered_set if the number of elements just 5 or 7.

--- num_common_elements.cpp
+++ num_common_elements.cpp
@@ -4,6 +4,7 @@
 #include <unordered_set> // for std::unordered_set
 #include <concepts> // for std::convertible_to<> concept
 #include <utility> // for std::declval
+#include <chrono>
 
 template<typename T>
 concept ReadOnlyList = requires(const T& list)
@@ -27,6 +28,7 @@ concept InsertableSet = requires(S<T>& set)
 template<ReadOnlyList T, template<typename> class S> requires InsertableSet<S, typename T::value_type>
 auto calculate_num_common_elements(const T& list1, const T& list2) noexcept
 {
+	std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
 	S<typename T::value_type> checkReg;
 	for(auto value : list1)
 		checkReg.insert(value);
@@ -34,6 +36,9 @@ auto calculate_num_common_elements(const T& list1, const T& list2) noexcept
 	for(auto value : list2)
 		if(checkReg.contains(value))
 			++count;
+	std::chrono::time_point<std::chrono::steady_clock> end = std::chrono::steady_clock::now();
+	double elapsedTime = std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(end - start).count();
+	std::cout << "Elapsed Time: " << elapsedTime << " milliseconds" << std::endl;
 	return count;
 }
 
@@ -53,16 +58,20 @@ static std::ostream& operator<<(std::ostream& stream, const T& list) noexcept
 int main()
 {
 	std::vector<int> list1 = { 5, 2, 8, 9, 4 };
+	for(std::size_t i = 0; i < 10000; ++i)
+		list1.push_back(2 * i + 1);
 	std::vector<int> list2 = { 3, 2, 9, 5 };
-	std::cout << "List1: " << list1 << std::endl;
-	std::cout << "List2: " << list2 << std::endl;
-	{
-		auto count = calculate_num_common_elements<decltype(list1), std::set>(list1, list2);
-		std::cout << "Result (set): " << count << std::endl;
-	}
+	for(std::size_t i = 0; i < 10000; ++i)
+		list2.push_back(2 * i);
+	//std::cout << "List1: " << list1 << std::endl;
+	//std::cout << "List2: " << list2 << std::endl;
 	{
 		auto count = calculate_num_common_elements<decltype(list1), std::unordered_set>(list1, list2);
 		std::cout << "Result (unordered_set): " << count << std::endl;
+	}
+	{
+		auto count = calculate_num_common_elements<decltype(list1), std::set>(list1, list2);
+		std::cout << "Result (set): " << count << std::endl;
 	}
 	return 0;
 };

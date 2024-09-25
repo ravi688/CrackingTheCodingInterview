fa3a89f5cf0d8f4addc8abab9a273f74d1281805
ravi688 <rp0412204@gmail.com>
2024-09-25 08:14:53 -0400

    Generalized traverse_subsets and gen_subsets

--- subsets_gen.cpp
+++ subsets_gen.cpp
@@ -2,13 +2,14 @@
 #include <vector> // for std::vector
 #include <concepts> // std::convertible_to
 #include <functional> // for std::function type erasure
+#include <utility> // for std::declval
 
 template<typename T>
 concept ReadOnlyContainer = requires(const T& container)
 {
 	container.cbegin();
 	container.cend();
-	{ container.size() } -> std::convertible_to<std::size_t>;
+	{ container.size() } -> std::integral;
 };
 
 
@@ -27,7 +28,17 @@ std::ostream& operator<<(std::ostream& stream, const T& container) noexcept
 	return stream;
 }
 
-void traverse_subsets(const std::vector<int>& set, std::size_t index, std::vector<int>& buf, std::function<void(void)>& callback) noexcept
+template<typename T>
+concept ReadOnlyVector = requires(const T& vector)
+{
+	typename T::value_type;
+	typename T::size_type;
+	{ vector.size() } -> std::convertible_to<typename T::size_type>;
+	vector.operator[](std::declval<typename T::size_type>());
+};
+
+template<ReadOnlyVector T>
+void traverse_subsets(const T& set, typename T::size_type index, std::vector<typename T::value_type>& buf, std::function<void(void)>& callback) noexcept
 {
 	if(index == set.size())
 	{
@@ -40,16 +51,17 @@ void traverse_subsets(const std::vector<int>& set, std::size_t index, std::vecto
 	traverse_subsets(set, index + 1, buf, callback);
 }
 
-std::vector<std::vector<int>> get_subsets(const std::vector<int>& set) noexcept
+template<ReadOnlyVector T>
+std::vector<std::vector<int>> get_subsets(const T& set) noexcept
 {
-	std::vector<std::vector<int>> subsets;
+	std::vector<std::vector<typename T::value_type>> subsets;
 	subsets.reserve(1 << set.size());
-	std::vector<int> buf;
+	std::vector<typename T::value_type> buf;
 	buf.reserve(set.size());
 	// NOTE: here std::function<void> results in in-complete type definition, so we are using std::function<void(void)>
 	std::function<void(void)> fn = [&buf, &subsets]()
 	{
-		std::vector<int> cloned = buf;
+		std::vector<typename T::value_type> cloned = buf;
 		subsets.push_back(std::move(cloned));
 	};
 	traverse_subsets(set, 0, buf, fn);

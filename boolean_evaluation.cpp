630f88fdedb7a94814dc35fb7f26f28ec2232a1a
ravi688 <rp0412204@gmail.com>
2025-01-15 18:40:03 +0530

    Added explanatory comments in boolean_evaluation.cpp

--- boolean_evaluation.cpp
+++ boolean_evaluation.cpp
@@ -13,25 +13,46 @@ static std::pair<std::size_t, std::size_t> evaluate(const std::string_view expre
 	}
 	std::size_t numWaysTrue = 0;
 	std::size_t numWaysFalse = 0;
+	// Build the expression tree for each operator as root (of a sub-tree)
 	for(std::size_t i = 0; const auto& ch : expression)
 	{
 		if(ch != '1' && ch != '0')
 		{
+			// Build the left expression tree
 			auto leftResult = evaluate(expression.substr(0, i));
+			// Build the right expression tree
 			auto rightResult = evaluate(expression.substr(i + 1));
 			switch(ch)
 			{
 				case '|':
+					// The number of ways of making 'a | b' true =
+					//	number of ways of making 'a' true * number of ways of making 'b' true
+					// 	+ number of ways of making 'a' true * number of ways of making 'b' false
+					// 	+ number of ways of making 'b' true * number of ways of making 'a' false
 					numWaysTrue += leftResult.first * rightResult.first
 							+ leftResult.first * rightResult.second + rightResult.first * leftResult.second;
+					// The number of ways of making 'a | b' false =
+					//	number of ways of making 'b' false * number of ways of making 'b' false
 					numWaysFalse += leftResult.second * rightResult.second;
 					break;
 				case '&':
+					// The number of ways of making 'a & b' true =
+					//	number of ways of making 'a' true * number of ways of making 'b' true
 					numWaysTrue += leftResult.first * rightResult.first;
+					// The number of ways of making 'a & b' false =
+					//	number of ways of making 'a' false * number of ways of making 'b' false
+					//	+ number of ways of making 'b' false * number of ways of making 'a' true
+					//	+ number of ways of making 'a' false * number of ways of making 'b' true
 					numWaysFalse += leftResult.second * rightResult.second
 							+ leftResult.second * rightResult.first + rightResult.second * leftResult.first;
 					break;
 				case '^':
+					// The number of ways of making 'a ^ b' true =
+					//	number of ways of making 'a' true * number of ways of making 'b' false
+					//	+ number of ways of making 'b' true * number of ways of making 'a' false
+					// The number of ways of making 'a ^ b' false =
+					//	number of ways of making 'a' true * number of ways of making 'b' true
+					//	+ number of ways of making 'b' false * number of ways of making 'b' false
 					numWaysTrue += leftResult.first * rightResult.second + rightResult.first * leftResult.second;
 					numWaysFalse += leftResult.first * rightResult.first + leftResult.second * rightResult.second;
 					break;

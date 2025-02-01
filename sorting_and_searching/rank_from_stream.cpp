93b4a74e8d395ad665b90d67653a7e80f5870025
ravi688 <rp0412204@gmail.com>
2025-02-01 16:48:56 +0530

    Added solution no 2 in rank_from_stream.cpp; Also fixed the previous (Solution no 1)

--- sorting_and_searching/rank_from_stream.cpp
+++ sorting_and_searching/rank_from_stream.cpp
@@ -21,16 +21,49 @@ class Solution1 : public ITracker<T>
 	private:
 		std::multiset<T> m_data;
 	public:
+		// Time complexity: Logarithmic
 		virtual void track(const T& value) noexcept override
 		{
 			m_data.insert(value);
 		}
+		// Time complexity: Logarithmic or Linear in the worst case because of std::distance(m_data.begin(), it)
 		virtual std::optional<std::size_t> getRankOfNumber(const T& value) noexcept override
 		{
-			auto it = m_data.find(value);
+			auto range = m_data.equal_range(value);
+			if(range.first == range.second)
+				return { };
+			return { std::distance(m_data.begin(), range.first) + std::distance(range.first, range.second) - 1 };
+		}
+};
+
+template<typename T>
+class Solution2 : public ITracker<T>
+{
+	private:
+		using RankData = std::pair<T, std::size_t>;
+		std::vector<RankData> m_data;
+	public:
+		virtual void track(const T& value) noexcept override
+		{
+			std::size_t thisRank = 0;
+			for(auto& rankData : m_data)
+				if(value < rankData.first)
+					rankData.second += 1;
+				else if(value > rankData.first)
+					thisRank += 1;
+				else
+				{
+					rankData.second += 1;
+					thisRank += 1;
+				}
+			m_data.push_back({ value, thisRank });
+		}
+		virtual std::optional<std::size_t> getRankOfNumber(const T& value) noexcept override
+		{
+			auto it = std::ranges::find_if(m_data, [&](const auto& _value) noexcept { return _value.first == value; });
 			if(it == m_data.end())
 				return { };
-			return { std::distance(m_data.begin(), it) };
+			return { it->second };
 		}
 };
 
@@ -63,7 +96,10 @@ static void runRankFromStream(const std::span<T> values)
 template<typename T>
 static void run(const std::span<T> values)
 {
+	std::cout << "**Solution no 1**\n";
 	runRankFromStream<Solution1, T>(values);
+	std::cout << "**Solution no 2**\n";
+	runRankFromStream<Solution2, T>(values);
 }
 
 int main()

299e4a7a12fd87bc1e994f6db23a5d3da6cad932
ravi688 <rp0412204@gmail.com>
2026-02-16 07:08:14 +0530

    Added solution for finding maximum length subarray having a given sum (unsigned variant)

--- dsa-practice-2026/array_problems/find_maximum_length_subarray_having_a_given_sum.cpp
+++ dsa-practice-2026/array_problems/find_maximum_length_subarray_having_a_given_sum.cpp
@@ -58,6 +58,61 @@ std::vector<int> find_max_len_subarray_2(const std::vector<int>& arr, int sum)
 	return std::vector<int>(std::next(arr.begin(), index), std::next(arr.begin(), index + len));
 }
 
+// Special case: for unsigned integers
+// Time complexity: O(n)
+// Space complexity: O(1)
+std::vector<unsigned int> find_max_len_subarray_3(const std::vector<unsigned int>& arr, unsigned sum)
+{
+	std::size_t right = 0;
+	std::size_t left = 0;
+	unsigned int _sum = 0;
+	std::size_t index = 0;
+	std::size_t len = 0;
+	while(right < arr.size())
+	{
+		if(_sum < sum)
+			_sum += arr[right++];
+		else if(_sum > sum && (left < right))
+			_sum -= arr[left++];
+		else // _sum == sum
+		{
+			if(len < (right - left))
+			{
+				len = right - left;
+				index = left;
+			}
+			if(right < arr.size())
+				_sum += arr[right++];
+		}
+	}
+	return std::vector<unsigned int>(std::next(arr.begin(), index), std::next(arr.begin(), index + len));
+}
+
+// Special case: for unsigned integers
+// Time complexity: O(n)
+// Space complexity: O(1)
+std::vector<unsigned int> find_max_len_subarray_4(const std::vector<unsigned int>& arr, unsigned sum)
+{
+	std::size_t len = 0, index = 0;
+	std::size_t left = 0, right = 0;
+	unsigned int _sum = 0;
+	for(; right < arr.size(); ++right)
+	{
+		_sum += arr[right];
+		while(_sum > sum && left < right)
+			_sum -= arr[left++];
+		if(_sum == sum)
+		{
+			if(len < (right - left + 1))
+			{
+				len = right - left + 1;
+				index = left; 
+			}
+		}
+	}
+	return std::vector<unsigned int>(std::next(arr.begin(), index), std::next(arr.begin(), index + len));
+}
+
 template<typename T>
 std::ostream& operator<<(std::ostream& stream, const std::vector<T>& v)
 {
@@ -77,5 +132,15 @@ int main()
 	std::cout << "-----Solution no 2-----\n";
 	subarr = find_max_len_subarray_2(arr, sum);
 	std::cout << "max length subarray: " << subarr << std::endl;
+	std::cout << "-----Solution no 3-----\n";
+	std::vector<unsigned int> uarr { 1, 2, 3, 2, 5, 1, 1, 2, 3, 2 };
+	std::cout << "input array: " << uarr << "\n";
+	std::cout << "sum: " << sum << "\n";
+	auto usubarr = find_max_len_subarray_3(uarr, static_cast<unsigned int>(sum));
+	std::cout<< "max length subarray: " << usubarr << "\n";
+	std::cout << "-----Solution no 4-----\n";
+	usubarr = find_max_len_subarray_4(uarr, static_cast<unsigned int>(sum));
+	std::cout << "max length subarray: " << usubarr << "\n";
 	return 0;
+
 }

ecad750332e0ccb632a9f2156287b3409da44e9b
ravi688 <rp0412204@gmail.com>
2026-06-06 15:16:01 +0530

    Added solution no 2 in split_linked_list_alternate_nodes.cpp

--- dsa-practice-2026/split_linked_list_alternate_nodes.cpp
+++ dsa-practice-2026/split_linked_list_alternate_nodes.cpp
@@ -13,37 +13,76 @@ struct LinkedListNode
 template<typename T>
 std::pair<LinkedListNode<T>*, LinkedListNode<T>*> splitLinkedList(LinkedListNode<T>* l)
 {
-    LinkedListNode<T>* secondHead = nullptr;
-    LinkedListNode<T>* secondTail = nullptr;
+	LinkedListNode<T>* secondHead = nullptr;
+	LinkedListNode<T>* secondTail = nullptr;
+	
+	auto curr = l;
+	LinkedListNode<T>* prev = nullptr;
+	bool alternate = false;
+	
+	while (curr)
+	{
+		if (alternate)
+		{
+			if (!secondHead)
+				secondHead = secondTail = curr;
+			else
+			{
+				secondTail->next = curr;
+				secondTail = curr;
+			}
+			prev->next = curr->next;
+		}
 
-    auto curr = l;
-    LinkedListNode<T>* prev = nullptr;
-    bool alternate = false;
+		prev = curr;
+		curr = curr->next;
+		alternate = !alternate;
+	}
+	
+	if (secondTail)
+		secondTail->next = nullptr;
+	
+	return { l, secondHead };
+}
 
-    while (curr)
-    {
-        if (alternate)
-        {
-            if (!secondHead)
-                secondHead = secondTail = curr;
-            else
-            {
-                secondTail->next = curr;
-                secondTail = curr;
-            }
+template<typename T>
+std::pair<LinkedListNode<T>*, LinkedListNode<T>*> splitLinkedList2(LinkedListNode<T>* l)
+{
+	LinkedListNode<T>* aHead = nullptr,
+		*aTail = nullptr,
+		*bHead = nullptr,
+		*bTail = nullptr;
 
-            prev->next = curr->next;
-        }
-
-        prev = curr;
-        curr = curr->next;
-        alternate = !alternate;
-    }
-
-    if (secondTail)
-        secondTail->next = nullptr;
-
-    return { l, secondHead };
+	bool isAlternate = false;
+	auto* head = l;
+	while(head)
+	{
+		if(isAlternate)
+		{
+			if(!bHead)
+				bHead = bTail = head;
+			else
+			{
+				bTail = bTail->next = head;
+			}
+		}
+		else
+		{
+			if(!aHead)
+				aHead = aTail = head;
+			else
+			{
+				aTail = aTail->next = head;
+			}
+		}
+		isAlternate = !isAlternate;
+		head = head->next;
+	}
+	if(aTail)
+		aTail->next = nullptr;
+	if(bTail)
+		bTail->next = nullptr;
+	return { aHead, bHead };
 }
 
 template<typename T>
@@ -81,9 +120,19 @@ int main(int argc, const char** argv)
 	printLinkedList(l);
 
 	std::cout << "Splitted linked lists: \n";
+	std::cout << "**--------- Solution no 1 -----------**\n";
 	const auto [s1, s2] = splitLinkedList(l);
 
 	printLinkedList(s1);
 	printLinkedList(s2);
+
+	std::cout << "**--------- Solution no 2 -----------**\n";
+
+	l = createLinkedList({1, 2, 3, 4, 5, 6, 7 });
+	const auto [_s1, _s2] = splitLinkedList2(l);
+
+	printLinkedList(_s1);
+	printLinkedList(_s2);
+
 	return 0;
 }
